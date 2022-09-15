4ba4e55d1f46ecde397ba6cfee05efa26d417fbc
Corey Steimel <coreysteimel@Coreys-MacBook-Pro-Work.local>
2022-09-15 09:48:04 -0600

    get git

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="BlogContainer">
+        <div className="Header">Simple Blog</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
